c2ce0f1fb2ab601998624d0c81f05b6e80a001af
Hadi Fooladi Talari <hadi.f7@gmail.com>
2022-08-22 14:42:37 -0400

    First Commit

---
+++ App/App/App/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace App
+{
+	public partial class MainPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_OnClicked(object sender, EventArgs e)
+		{
+			_contentView.Content = new Label();
+			_contentView.Content = _roundedButton;
+		}
+	}
+}
